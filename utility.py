365a6072d057d5cc3d750a4ec5d2a8e3b927f553
mattbevenour <giftsungiven67@gmail.com>
2023-12-18 23:42:52 -0500

    feat: data extend
    added additional functionality for more players, absent players, various player sizes

--- utility.py
+++ utility.py
@@ -7,7 +7,7 @@ from pyaml_env import parse_config
 config = parse_config('config.yaml')
 target_variable = config['target_variable']
 
-def populate_games(num_games: int, playerbase: list, df: pd.DataFrame) -> pd.DataFrame:
+def populate_games(num_games: int, playerbase: list, df: pd.DataFrame, players_per_team: int = 5) -> pd.DataFrame:
     '''
     Main game generation function
 
@@ -20,18 +20,22 @@ def populate_games(num_games: int, playerbase: list, df: pd.DataFrame) -> pd.Dat
     df['game_id'] = list(range(1,num_games+1))
 
     # generate dictionary for team rosters based on game ID
-    rosters = generate_rosters(num_games, playerbase)
+    rosters = generate_rosters(num_games, playerbase, 2 * players_per_team)
 
     # take rosters and assign players on roster to team 1
     # remaining 5 players will be assigned team 0
     for game_id in rosters.keys():
-        home_team = rosters[game_id]
+        home_team = rosters[game_id][:players_per_team]
+        away_team = rosters[game_id][players_per_team:]
         for player in home_team:
-          df.loc[df['game_id']==game_id, [f'{player}_team']] = 1
+          df.loc[df['game_id']==game_id, [f'{player}_team1']] = 1
+        for player in away_team:
+          df.loc[df['game_id']==game_id, [f'{player}_team0']] = 1
     
-    # fill null teams with team 0
+    # fill nulls
     for player in playerbase:
-        df[f'{player}_team'].fillna(0, inplace=True)
+        df[f'{player}_team0'].fillna(0, inplace=True)
+        df[f'{player}_team1'].fillna(0, inplace=True)
     
     # calculate each players' goal scoring per game
     df_scores = generate_goals(playerbase, df)
@@ -45,21 +49,24 @@ def populate_games(num_games: int, playerbase: list, df: pd.DataFrame) -> pd.Dat
     # return complete game list
     return df_scores
 
-def generate_rosters(x: int, p: list)-> dict:
+def generate_rosters(x: int, p: list, num_sample: int = 5)-> dict:
     '''
-    Generates teams of 5 for team 1 for X games
+    Generates teams of num_sample for team 1 for X games
     Players are chosen from from exhaustive list of players p
     '''
     return_dict ={}
     for y in range(1,x+1):
-        return_dict[y]= sample(p,5)
+        return_dict[y]= sample(p,num_sample)
     return return_dict
 
 def generate_goals(playerbase: list, df: pd.DataFrame) -> pd.DataFrame:
     for player in playerbase:
-        df[f'{player}_goals']= np.random.randint(config['scoring_ranges'][player][0],
-                                                 config['scoring_ranges'][player][1] +1,
+        df[f'{player}_goals']= np.where((df[f'{player}_team0'] == 0) & (df[f'{player}_team1'] == 0),
+                                        0,
+                                        np.random.randint(config['scoring_ranges_extended'][player][0],
+                                                 config['scoring_ranges_extended'][player][1] +1,
                                                   len(df))
+        )
     return df
 
 def calc_game_score(playerbase: list, num_games: int, df: pd.DataFrame) -> pd.DataFrame:
@@ -67,9 +74,9 @@ def calc_game_score(playerbase: list, num_games: int, df: pd.DataFrame) -> pd.Da
         team1_total = 0
         team0_total = 0
         for player in playerbase:
-            if df.loc[df['game_id']==x, [f'{player}_team']].values[0] == 1:
+            if df.loc[df['game_id']==x, [f'{player}_team1']].values[0] == 1:
                 team1_total = team1_total + df.loc[df['game_id'] ==x, [f'{player}_goals']].values[0]
-            else:
+            elif df.loc[df['game_id']==x, [f'{player}_team0']].values[0] == 1:
                 team0_total = team0_total + df.loc[df['game_id'] ==x, [f'{player}_goals']].values[0]
         df.loc[df['game_id']==x, ['score_difference']] = int(team1_total - team0_total)
     return df
